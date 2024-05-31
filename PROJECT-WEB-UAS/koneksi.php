51f274bece8676a1a49631aa198046d227e33f28
Andrian <161455418+andriandwiii@users.noreply.github.com>
2024-05-31 07:38:45 +0700

    Delete PROJECT-WEB-UAS directory

--- PROJECT-WEB-UAS/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $koneksi = mysqli_connect('localhost', 'root', '' , 'uas');
-
-    if(!$koneksi)
-    {
-        die ("Koneksi dengan database gagal: " . mysqli_connect_errno() . " " . mysqli_connect_error());
-    }
-?>
